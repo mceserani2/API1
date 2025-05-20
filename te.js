1b2f2e5c15b391ca93246df63bfc2a8336b5ecb7
Matteo Ceserani <matteo.ceserani@polomanettiporciatti.edu.it>
2025-05-20 16:36:03 +0200

    Aggiunto post /Te

--- te.js
+++ te.js
@@ -18,16 +18,47 @@ app.get("/abb/cibo/:nomeCibo", (req, res) => {
     // Eseguire una query sul DB per ottenere tutti gli abbinamenti
     // per la variabile nomeCibo e inviarli in JSON
     const nomeCibo = req.params.nomeCibo;
-    const query = `SELECT * FROM Cibo AS C INNER JOIN Abbinamento AS A ON C.Id = A.IdCibo INNER JOIN Te AS T ON T.Id = A.IdTe WHERE C.Nome = "${nomeCibo}"`;
+    const query = `SELECT T.Id, T.Nome, T.Tipologia, T.Descrizione FROM Cibo AS C INNER JOIN Abbinamento AS A ON C.Id = A.IdCibo INNER JOIN Te AS T ON T.Id = A.IdTe WHERE C.Nome = "${nomeCibo}"`;
     db.all(query, (err, rows) => {
         if (err) {
             console.log("Errore di esecuzione della query");
             return res.status(500).json({errore: err.message})
         }
-        if (!rows || rows.length === 0) {
+        if (!rows || rows.length === 0)      {
             return res.status(404).json({ errore: "Nessun abbinamento trovato"});
         }
         res.json(rows);
+    });
+});
+
+app.get("/abb/te/:nomeTe", (req, res) => {
+    // Eseguire una query sul DB per ottenere tutti gli abbinamenti
+    // per la variabile nomeTe e inviarli in JSON
+    const nomeTe = req.params.nomeTe;
+    const query = `SELECT C.Id, C.Nome, C.Tipologia, C.Descrizione FROM Te AS T INNER JOIN Abbinamento AS A ON T.Id = A.IdTe INNER JOIN Cibo AS C ON C.Id = A.IdCibo WHERE T.Nome = "${nomeTe}"`;
+    db.all(query, (err, rows) => {
+        if (err) {
+            console.log("Errore di esecuzione della query");
+            return res.status(500).json({errore: err.message})
+        }
+        if (!rows || rows.length === 0)      {
+            return res.status(404).json({ errore: "Nessun abbinamento trovato"});
+        }
+        res.json(rows);
+    });
+});
+
+app.post("/te",(req, res) => {
+    const { Nome, Tipologia, Descrizione } = req.body;
+    if (!Nome || Nome.length === 0 || !Tipologia || Tipologia.length === 0) {
+        return res.status(400).json({ "Errore": "Nome e Tipologia sono obbligatori"});
+    }
+    const query = `INSERT INTO Te (Nome, Tipologia, Descrizione) VALUES ("${Nome}","${Tipologia}","${Descrizione}");`;
+    db.run(query,(err) => {
+        if (err) {
+            return res.status(500).json({ "Errore": err.message });
+        }
+        res.status(201).json({ "Messaggio" : "te inserito con successo"});
     });
 });
 
