4235b701776abf7ea40dac56c53c47c2aef871ef
Matteo Ceserani <matteo.ceserani@polomanettiporciatti.edu.it>
2025-05-20 18:14:52 +0200

    Post implementate

--- te.js
+++ te.js
@@ -62,6 +62,49 @@ app.post("/te",(req, res) => {
     });
 });
 
+app.post("/cibo",(req, res) => {
+    const { Nome, Tipologia, Descrizione } = req.body;
+    if (!Nome || Nome.length === 0 || !Tipologia || Tipologia.length === 0) {
+        return res.status(400).json({ "Errore": "Nome e Tipologia sono obbligatori"});
+    }
+    const query = `INSERT INTO Cibo (Nome, Tipologia, Descrizione) VALUES ("${Nome}","${Tipologia}","${Descrizione}");`;
+    db.run(query,(err) => {
+        if (err) {
+            return res.status(500).json({ "Errore": err.message });
+        }
+        res.status(201).json({ "Messaggio" : "cibo inserito con successo"});
+    });
+});
+
+app.post("/abb",(req,res) => {
+    const { nomeCibo, nomeTe, consigliato } = req.body;
+    if (!nomeCibo || nomeCibo.length === 0 || !nomeTe || nomeTe.length === 0 || (consigliato !== 0 && consigliato !== 1)){
+        return res.status(400).json({ "Errore" : "parametri non validi"});
+    }
+    const te = `SELECT T.Id FROM Te AS T WHERE T.Nome = "${nomeTe}"`;
+    const cibo = `SELECT C.Id FROM Cibo AS C WHERE C.Nome = "${nomeCibo}"`;
+    db.get(te,(errTe, rowTe) => {
+        if (errTe) {
+            return res.status(500).json({ "Errore": errTe.message });
+        }
+        db.get(cibo,(errCibo,rowCibo) => {
+            if (errCibo) {
+                return res.status(500).json({ "Errore": errCibo.message });
+            }
+            if (!rowTe || !rowCibo){
+                return res.status(404).json({ "Errore": "Te e/o Cibo non presenti" });
+            }
+            const insAbb = `INSERT INTO Abbinamento VALUES (${rowCibo.Id},${rowTe.Id},${consigliato})`;
+            db.run(insAbb,(err) => {
+                if (err) {
+                    return res.status(500).json({ "Errore": err.message });
+                }
+                res.status(201).json({ "Messaggio": "Abbinamento inserito con successo"}); 
+            });
+        });
+    });
+});
+
 app.listen(PORT,() => {
     console.log(`Server in ascolo sulla porta ${PORT}`);
 });
